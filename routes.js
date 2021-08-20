3cac75e3764cf652e97493ff6de3002250d77b97
Rafadcosta <rafaeladuqu@gmail.com>
2021-08-19 22:35:04 -0300

    routes

--- routes.js
+++ routes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', function(res, res) {
-    return res.render("layout");
+    return res.redirect("/instructors");
+})
+
+routes.get('/instructors', function(res, res) {
+    return res.render("instructors/index");
+})
+
+routes.get('/members', function(res, res) {
+    return res.send("members");
 })
 
 module.exports = routes;
