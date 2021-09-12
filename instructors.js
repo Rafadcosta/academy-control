71ec3b7bfc4097fac717baed8451d0e69edebd1f
Rafaela Duque <47401318+Rafadcosta@users.noreply.github.com>
2021-09-12 06:26:11 -0300

    formating json data

--- instructors.js
+++ instructors.js
@@ -12,7 +12,7 @@ exports.post = function(req, res) {
 
     data.instructors.push(req.body); 
 
-    fs.writeFile("data.json", JSON.stringify(data), function(err) {
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("Write file error!")
 
         return res.redirect("instructors");
