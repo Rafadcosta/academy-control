8f499486cbdd3a62602c8bbcc7405cf4f8424667
Rafaela Duque <47401318+Rafadcosta@users.noreply.github.com>
2021-09-12 05:59:40 -0300

    validate data on backend

--- routes.js
+++ routes.js
@@ -14,6 +14,14 @@ routes.get('/instructor/create', function(req, res) {
 });
 
 routes.post("/instructors", function(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+        if (req.body[key] == ""){
+            return res.send("Por favor, preencha todos os campos!");
+        }
+    }
+
     return res.send(req.body);
 });
 
